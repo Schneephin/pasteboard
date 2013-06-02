571ebb64623f1539d1dc490dfc1359ca8b7cc8a8
Anja Siek <ja.sie.kan@googlemail.de>
2013-06-02 20:10:59 +0200

    add validation function to form

--- www/js/pages/register.js
+++ www/js/pages/register.js
@@ -9,25 +9,28 @@
  * @param page
  */
 function handleRegister(form, page) {
-    var result = handleForm(form, page);
-    
-    if (result["status"] == 0) {
-        if (result["result"].state == 'ok') {
-            if (null != result["result"].data.token) {
-                this.document.location.href = "pastes.py?tk="+result["result"].data.token;
+
+    if (pruefeFormular(form)) {
+        var result = handleForm(form, page);
+        
+        if (result["status"] == 0) {
+            if (result["result"].state == 'ok') {
+                if (null != result["result"].data.token) {
+                    this.document.location.href = "pastes.py?tk="+result["result"].data.token;
+                } else {
+                    errorHandler.handel("failed to load data");n
+                }
             } else {
-                errorHandler.handel("failed to load data");n
+                if (null != result["result"].msg) {
+                    errorHandler.handel(result["result"].msg);
+                } else {
+                    errorHandler.handel("failed to load data");
+                }
             }
         } else {
-            if (null != result["result"].msg) {
-                errorHandler.handel(result["result"].msg);
-            } else {
-                errorHandler.handel("failed to load data");
-            }
+            errorHandler.handel("your register-Data was not correct");
         }
-    } else {
-        errorHandler.handel("your register-Data was not correct");
-    }
+    } 
 }
 /**
  * function handleLogin
--- www/templates/register.tpl
+++ www/templates/register.tpl
@@ -3,11 +3,11 @@
     <div id ="info"><%=trans.text%></div>
     <div id="register-form">
         <form onsubmit="handleRegister(this,\'pasteboard.py/register'); return false;">
-            <input type="text" name="name" placeholder="<%=trans.name%>">
-            <input type="text" name="email" placeholder="<%=trans.header.email%>">
-            <input type="password" name="password" placeholder="<%=trans.header.password%>">
-            <input type="password" name="password-r" placeholder="<%=trans.passwordr%>">
-            <input type="text" name="key" placeholder="<%=trans.key%>">
+            <input class="test" type="text" name="name" placeholder="<%=trans.name%>">
+            <input class="test" type="text" name="email" placeholder="<%=trans.header.email%>">
+            <input class="test" type="password" name="password" placeholder="<%=trans.header.password%>">
+            <input class="test" type="password" name="password-r" placeholder="<%=trans.passwordr%>">
+            <input class="test" type="text" name="key" placeholder="<%=trans.key%>">
             <input type="submit" value="<%=trans.header.ok%>">
         </form> 
     </div>
